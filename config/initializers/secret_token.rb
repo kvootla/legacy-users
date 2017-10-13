3d9cba4d70c1379cd003818486710f491180ff65
kvootla <vootla.krishna@gmail.com>
2017-10-13 13:02:49 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LegacyUsers::Application.config.secret_key_base = 'e368ec8471d3aa01cf3b2b3768e4b062af969a790eb3a197c52d21b8ec6ed5ab19a85378b787a02fc9c95707097bd4fc3faff4f938a49e2f9ce0e2271d13702e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LegacyUsers::Application.config.session_store :cookie_store, key: '_legacy-users_session'
